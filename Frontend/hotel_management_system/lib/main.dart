cc5a2e011558ef7c372cc5d4c64bab09e8203ac0
Kamil <kamilpier08@gmail.com>
2021-05-24 13:28:27 +0200

    FrontPageAppBar

--- Frontend/hotel_management_system/lib/main.dart
+++ Frontend/hotel_management_system/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hotel_management_system/components/MainContainer.dart';
+import 'package:hotel_management_system/pages/frontPage/FrontPage.dart';
 import 'package:hotel_management_system/utils/colorTheme.dart';
 
 void main() {
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: colorTheme.themeData,
       home: MainContainer(
-        child: Text("elo"),
+        child: FrontPage(),
       ),
     );
   }
